1baeafa388b8d01d48d1fe38d099105738c99cb2
Андрей Шестаков <GlobalShow6@Gmail.com>
2022-02-22 09:52:49 +0300

    FindText: поправил замечания, исправил тесты.

--- FindText/FindText.cpp
+++ FindText/FindText.cpp
@@ -3,64 +3,71 @@
 #include <functional>
 #include <fstream>
 #include <string>
+#include <Windows.h>
 
 using namespace std;
-using FindStringCallback = function<void(int lineIndex, const string& line, size_t foundPos)>;
+using FindStringCallback = function<void(size_t lineIndex, const string& line, size_t foundPos)>;
 
-bool FindStringInStream(
-	istream& fileContent,
+ifstream GetStreamFromFile(const char* filePath)
+{
+	ifstream file(filePath);
+	if (!file.is_open())
+	{
+		throw invalid_argument("Failed to open file for searching.");
+	}
+	return file;
+}
+
+bool FindStringInFile(
+	const char* file,
 	const string& needle,
 	const FindStringCallback& callback = FindStringCallback())
 {
-	//зависает функция 
+	ifstream fileStream = GetStreamFromFile(file);
 	string line;
 	bool found = false;
-	for (int lineIndex = 1; getline(fileContent, line); ++lineIndex)
+	for (size_t lineIndex = 1; getline(fileStream, line); lineIndex++)
 	{
-		size_t pos;
-		while ((pos = line.find(needle)) != string::npos)
+		size_t pos = line.find(needle, 0);
+		while (pos != string::npos)
 		{
 			found = true;
 			if (callback) callback(lineIndex, line, pos);
+			pos = line.find(needle, pos + 1);
 		}
 	}
 	return found;
-}
+} 
 
-void PrintFoundLineIndex(int lineIndex, string const& line, size_t foundPos)
+void PrintFoundLineIndex(size_t lineIndex, string const& line, size_t foundPos)
 {
-	cout << "lineIndex: " << lineIndex << "; Line: " << line << "; foundPos: "<< foundPos << ";" << endl;
+	cout << "lineIndex: " << lineIndex << ";" << " foundPos: " << foundPos << ";" << " line: " << line << endl;
 }
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
+	setlocale(LC_ALL, "Rus");
+
+	try
 	{
-		cout << "Invalid arguments count." << endl
-			 << "Usage: findtext.exe <file name> <text to search>" << endl;
+		if (argc != 3)
+		{
+			cout << "Invalid arguments count." << endl
+				<< "Usage: findtext.exe <file name> <text to search>" << endl;
+			return 1;
+		}
+
+		if (!FindStringInFile(argv[1] /*file*/, argv[2] /*NeedToFind*/, PrintFoundLineIndex))
+		{
+			cout << "Text not found." << endl;
+			return 1;
+		}
+	}
+	catch (const exception& e)
+	{
+		cout << e.what() << endl;
 		return 1;
 	}
-
-	ifstream file(argv[1]);
-	if (!file.is_open())
-	{
-		cout << "Failed to open " << argv[1] << " for searching." << endl;
-		return 1;
-	}
-	//можно сразу передать файл
-	stringstream buffer;
-	buffer << file.rdbuf();
-
-	file.close();
-
-	if (!FindStringInStream(buffer, argv[2], PrintFoundLineIndex))
-	{
-		cout << "Text not found." << endl;
-		return 1;
-	}
-
-	buffer.clear();
-	buffer.seekg(0, ios_base::beg);
 
 	return 0;
 }
--- FindText/auto_test.bat
+++ FindText/auto_test.bat
@@ -14,13 +14,13 @@ fc.exe "%TEMP%\more_match_in_string_output.txt" test_files\more_match_in_string_
 if ERRORLEVEL 1 goto err
 
 rem ожидаем нулевой код завершения программы если есть несколько совпадений в разных строках.
-%PROGRAM% test_files\more_match_in_multi_string.txt test > "%TEMP%\more_match_in_multi_string_output.txt"
-if NOT ERRORLEVEL 1 goto err
+%PROGRAM% test_files\more_match_in_multi_string.txt cat > "%TEMP%\more_match_in_multi_string_output.txt"
+if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\more_match_in_multi_string_output.txt" test_files\more_match_in_multi_string_result.txt
 if ERRORLEVEL 1 goto err
 
 rem ожидаем ненулевой код, если в файле нет текста.
-%PROGRAM% test_files\file_empty.txt dog > "%TEMP%\file_empty_output.txt"
+%PROGRAM% test_files\file_empty.txt cat > "%TEMP%\file_empty_output.txt"
 if NOT ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\file_empty_output.txt" test_files\file_empty_result.txt
 if ERRORLEVEL 1 goto err
@@ -28,22 +28,26 @@ if ERRORLEVEL 1 goto err
 rem ожидаем ненулевой код, если нет совпадений в одной-строчном файле.
 %PROGRAM% test_files\non_match_in_string.txt dog > "%TEMP%\non_match_in_string_output.txt"
 if NOT ERRORLEVEL 1 goto err
-fc.exe "%TEMP%\non_match_in_string_output.txt" test_files\non_match_in_string_result.txt
+fc.exe "%TEMP%\non_match_in_string_output.txt" test_files\file_empty_result.txt
 if ERRORLEVEL 1 goto err
 
 rem ожидаем ненулевой код, если нет совпадений в много-строчном файле.
 %PROGRAM% test_files\non_match_in_multi_string.txt dog > "%TEMP%\non_match_in_multi_string_output.txt"
 if NOT ERRORLEVEL 1 goto err
-fc.exe "%TEMP%\non_match_in_multi_string_output.txt" test_files\non_match_in_multi_string_result.txt
+fc.exe "%TEMP%\non_match_in_multi_string_output.txt" test_files\file_empty_result.txt
 if ERRORLEVEL 1 goto err
 
 rem ожидаем ненулевой код, если файла не существует.
-%PROGRAM% non-existing-file-name.txt arg 
+%PROGRAM% non-existing-file-name.txt arg > "%TEMP%\non_existing_file_name_output.txt"
 if NOT ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\non_existing_file_name_output.txt" test_files\file_missing_result.txt
+if ERRORLEVEL 1 goto err
 
 rem ожидаем ненулевой код, если не верные аргументы
-%PROGRAM% non-existing-file-name.txt
+%PROGRAM% test_files\one_match_in_string.txt > "%TEMP%\file_missing_argv_output.txt"
 if NOT ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\file_missing_argv_output.txt" test_files\file_missing_argv_result.txt
+if ERRORLEVEL 1 goto err
 
 echo Program testing succeeded
 exit 0
