01af15c0535c62eebecc4a63d097fd09d765f110
Андрей Шестаков <GlobalShow6@Gmail.com>
2022-03-07 19:30:56 +0300

    InvertMatrix: добавил заготовку для тестов

--- InvertMatrix/InvertMatrix.cpp
+++ InvertMatrix/InvertMatrix.cpp
@@ -10,63 +10,6 @@ namespace
 	using Matrix = std::vector<std::vector<double>>;	
 }
 
-void CheckArguments(int argc);
-std::ifstream GetFileStream(std::string const& filePath);
-Matrix FillMatrix(std::ifstream& fileContent);
-Matrix ReadMatrixFromFile(std::string const& filePath);
-void PrintMatrix(Matrix const& matrix);
-Matrix InvertMatrix(Matrix const& matrix);
-void StartInvertMatrix(int argc, char* argv[]);
-Matrix MatrixTranspose(Matrix const& matrix);
-Matrix MinorByElementInMatrix(Matrix const& matrix, size_t elemRow, size_t elemCol);
-double MatrixDeterminant(Matrix const& matrix);
-double MinorOfElementInMatrix(Matrix const& matrix, size_t elemRow, size_t elemCol);
-Matrix MatrixAlgebraicAdditions(Matrix const& matrix);
-Matrix InvertMatrix(Matrix const& matrix);
-
-void CheckArguments(int argc)
-{
-	if (argc != 2)
-	{
-		std::cout << "Invalid arguments count." << std::endl
-			<< "\tUsage: findtext.exe <matrix filePath>" << std::endl
-			<< "\t\t<matrix filePath> - path to file, where matrix coefficients are stored." << std::endl;
-		throw std::invalid_argument("Invalid arguments count.");
-	}
-}
-std::ifstream GetFileStream(std::string const& filePath)
-{
-	std::ifstream file(filePath);
-	if (!file.is_open())
-	{
-		throw std::invalid_argument("Failed to open file for searching.");
-	}
-	return file;
-}
-Matrix FillMatrix(std::ifstream& fileContent)
-{
-	Matrix result;
-	std::string line;
-	while (std::getline(fileContent, line)) 
-	{
-		std::istringstream iss { line };
-		std::vector<double> tempv;	
-		double element;
-
-		while (iss >> element)
-		{
-			tempv.push_back(element);
-		}
-		result.push_back(tempv);
-	}
-	fileContent.close();
-	return result;
-}
-Matrix ReadMatrixFromFile(std::string const& filePath)
-{
-	std::ifstream fileStream = GetFileStream(filePath);
-	return FillMatrix(fileStream);
-}
 void PrintMatrix(Matrix const& matrix)
 {
 	for (auto const& row : matrix)
@@ -77,37 +20,6 @@ void PrintMatrix(Matrix const& matrix)
 		}
 		std::cout << std::endl;
 	}
-}
-Matrix InvertMatrix(Matrix const& matrix)
-{
-	double determinant = MatrixDeterminant(matrix);
-
-	if (determinant == 0)
-	{
-		throw std::logic_error("Inverse doesn't exist.");
-	}
-
-	Matrix matrixTranspose = MatrixTranspose(matrix);
-	Matrix algebraicAdditionsMatrix = MatrixAlgebraicAdditions(matrixTranspose);
-	Matrix InvertMatrix;
-
-	for (size_t i = 0; i < matrix.size(); i++)
-	{
-		std::vector<double> rowMatrix;
-		for (size_t j = 0; j < matrix.size(); j++)
-		{
-			rowMatrix.push_back(algebraicAdditionsMatrix[i][j] / determinant);
-		}
-		InvertMatrix.push_back(rowMatrix);
-	}
-	return InvertMatrix;
-}
-void StartInvertMatrix(int argc, char* argv[])
-{
-	CheckArguments(argc);
-	Matrix matrix = ReadMatrixFromFile(argv[1]);
-	Matrix invertMatrix = InvertMatrix(matrix);
-	PrintMatrix(invertMatrix);
 }
 Matrix MatrixTranspose(Matrix const& matrix)
 {
@@ -181,6 +93,80 @@ Matrix MatrixAlgebraicAdditions(Matrix const& matrix)
 		result.push_back(col);
 	}
 	return result;
+}
+Matrix InvertMatrix(Matrix const& matrix)
+{
+	double determinant = MatrixDeterminant(matrix);
+
+	if (determinant == 0)
+	{
+		throw std::logic_error("Inverse doesn't exist.");
+	}
+
+	Matrix matrixTranspose = MatrixTranspose(matrix);
+	Matrix algebraicAdditionsMatrix = MatrixAlgebraicAdditions(matrixTranspose);
+	Matrix InvertMatrix;
+
+	for (size_t i = 0; i < matrix.size(); i++)
+	{
+		std::vector<double> rowMatrix;
+		for (size_t j = 0; j < matrix.size(); j++)
+		{
+			rowMatrix.push_back(algebraicAdditionsMatrix[i][j] / determinant);
+		}
+		InvertMatrix.push_back(rowMatrix);
+	}
+	return InvertMatrix;
+}
+Matrix FillMatrix(std::ifstream& fileContent)
+{
+	Matrix result;
+	std::string line;
+	while (std::getline(fileContent, line))
+	{
+		std::istringstream iss{ line };
+		std::vector<double> tempv;
+		double element;
+
+		while (iss >> element)
+		{
+			tempv.push_back(element);
+		}
+		result.push_back(tempv);
+	}
+	fileContent.close();
+	return result;
+}
+std::ifstream GetFileStream(std::string const& filePath)
+{
+	std::ifstream file(filePath);
+	if (!file.is_open())
+	{
+		throw std::invalid_argument("Failed to open file for searching.");
+	}
+	return file;
+}
+Matrix ReadMatrixFromFile(std::string const& filePath)
+{
+	std::ifstream fileStream = GetFileStream(filePath);
+	return FillMatrix(fileStream);
+}
+void CheckArguments(int argc)
+{
+	if (argc != 2)
+	{
+		std::cout << "Invalid arguments count." << std::endl
+			<< "\tUsage: findtext.exe <matrix filePath>" << std::endl
+			<< "\t\t<matrix filePath> - path to file, where matrix coefficients are stored." << std::endl;
+		throw std::invalid_argument("Invalid arguments count.");
+	}
+}
+void StartInvertMatrix(int argc, char* argv[])
+{
+	CheckArguments(argc);
+	Matrix matrix = ReadMatrixFromFile(argv[1]);
+	Matrix invertMatrix = InvertMatrix(matrix);
+	PrintMatrix(invertMatrix);
 }
 
 int main(int argc, char* argv[])
