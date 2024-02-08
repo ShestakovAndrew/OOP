336ea55e508ebe5bdb0381853dd8221449607ef5
Andrew <globalshow6@gmail.com>
2024-02-09 01:14:53 +0300

    BMPInfo: дополнил файл описывающий header bmp файла

--- Lab_1/Task_4/BMPInfo/BmpFileHeader.h
+++ Lab_1/Task_4/BMPInfo/BmpFileHeader.h
@@ -2,33 +2,32 @@
 #include <cstdint>
 #include <array>
 
-static const uint8_t BMP_SIGNATURE[] = { 0x42, 0x4D };
+static const uint8_t BMP_SIGNATURE_BE[] = { 0x42, 0x4D };
+static const uint8_t BMP_SIGNATURE_LE[] = { 0x4D, 0x42 };
+static const std::vector<uint16_t> AVAILABLE_IMAGE_BITS = { 1, 2, 4, 8, 16, 24, 32, 48, 64 };
 
 enum class BMPCompression : uint16_t
 {
 	RGB = 0,
-	RLE8 = 1,
-	RLE4 = 2,
-	BITFIELDS = 3,
+	RLE_8 = 1,
+	RLE_4 = 2,
+	BIT_FIELDS = 3,
 	JPEG = 4,
 	PNG = 5,
-	ALPHABITFIELDS = 6,
-	CMYK = 11,
-	CMYKRLE8 = 12,
-	CMYKRLE4 = 13
+	ALPHA_BIT_FIELDS = 6
 };
 
 #pragma pack (push, 1)
 struct BitmapFileHeader
 {
-	uint8_t signature[sizeof(BMP_SIGNATURE)];
-	uint32_t size;
+	uint8_t type[sizeof(BMP_SIGNATURE_BE)];
+	uint32_t fileSize;
 	uint32_t reserved;
 	uint32_t offBits;
 };
 struct BitmapInfoHeader
 {
-	uint32_t size;
+	uint32_t sizeInfoHeader;
 	int32_t width;
 	int32_t height;
 	uint16_t planes;
@@ -40,9 +39,28 @@ struct BitmapInfoHeader
 	uint32_t colorUsed;
 	uint32_t colorImportant;
 };
+struct BitmapV4Header : BitmapInfoHeader
+{
+	uint32_t redMask;
+	uint32_t greenMask;
+	uint32_t blueMask;
+	uint32_t alphaMask;
+	uint32_t csType;
+	CIEXYZTRIPLE endpoints;
+	uint32_t gammaRed;
+	uint32_t gammaGreen;
+	uint32_t gammaBlue;
+};
+struct BitmapV5Header : BitmapV4Header
+{
+	uint32_t intent;
+	uint32_t profileData;
+	uint32_t profileSize;
+	uint32_t reserved;
+};
 struct BMPInfo
 {
 	BitmapFileHeader bmpFileHeader;
-	BitmapInfoHeader bmpInfoHeader;
+	BitmapV5Header bmpInfoHeader;
 };
 #pragma pack (pop)
