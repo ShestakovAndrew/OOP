5ad0ce6b4ce7826a16e2f5256d3e9c577b088394
Andrew <globalshow6@gmail.com>
2024-02-11 00:48:33 +0300

    RLE: добавил описание тестов

---
+++ Lab_1/Task_4/RLE/auto_test.bat
@@ -0,0 +1,65 @@
+rem Переменная PROGRAM будет хранить первый аргумент командной строки заключённый в кавычки
+set PROGRAM="%~1"
+
+rem ожидаем нулевой код если файл содержит 255 байт одинаковых символов подряд 
+	%PROGRAM% pack test_files\input_255_insuccession.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\result.txt" test_files\result_255_insuccession.txt
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_successfully.txt
+	if ERRORLEVEL 1 goto err
+
+rem ожидаем нулевой код если файл содержит 256 байт одинаковых символов подряд 
+	%PROGRAM% pack test_files\input_256_insuccession.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\result.txt" test_files\result_256_insuccession.txt
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_successfully.txt
+	if ERRORLEVEL 1 goto err
+
+rem ожидаем нулевой код если файл содержит 257 байт одинаковых символов подряд 
+	%PROGRAM% pack test_files\input_257_insuccession.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\result.txt" test_files\result_257_insuccession.txt
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_successfully.txt
+	if ERRORLEVEL 1 goto err
+
+rem ожидаем ненулевой код если файл содержит символ с кодом 255
+	%PROGRAM% pack test_files\input_alpha_255.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if NOT ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_alpha_error.txt
+	if ERRORLEVEL 1 goto err
+
+rem ожидаем ненулевой код если длина запакованного файла нечётная
+	%PROGRAM% unpack test_files\input_odd_pack.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if NOT ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_odd_error.txt
+	if ERRORLEVEL 1 goto err
+
+rem ожидаем ненулевой код если в запакованном файле кол-во повторений символа, равное нулю
+	%PROGRAM% unpack test_files\input_alpha_zero_pack.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if NOT ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_zero_pack_error.txt
+	if ERRORLEVEL 1 goto err
+
+rem ожидаем ненулевой код если файл нулевой длины
+	%PROGRAM% pack test_files\input_file_empty.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if NOT ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\result.txt" test_files\input_file_empty.txt
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_empty_file.txt
+	if ERRORLEVEL 1 goto err
+
+rem ожидаем ненулевой код если нет аргумента
+	%PROGRAM% test_files\input_255_insuccession.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if NOT ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_argv_error.txt
+	if ERRORLEVEL 1 goto err
+
+echo Program testing succeeded
+exit 0
+
+:err
+echo Program testing failed
+exit 1
