32428a019917f1a78b14635f8847c6a7ed872fda
Andrew <globalshow6@gmail.com>
2024-02-21 00:52:56 +0300

    RLE: поправил тесты

--- Lab_1/Task_4/RLE/auto_test.bat
+++ Lab_1/Task_4/RLE/auto_test.bat
@@ -1,12 +1,34 @@
 rem Переменная PROGRAM будет хранить первый аргумент командной строки заключённый в кавычки
 set PROGRAM="%~1"
 
+rem ожидаем нулевой код если файл нулевой длины
+	%PROGRAM% pack test_files\input_file_empty.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\result.txt" test_files\input_file_empty.txt
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_pack_successfully.txt
+	if ERRORLEVEL 1 goto err
+
+	%PROGRAM% unpack test_files\input_file_empty.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\result.txt" test_files\input_file_empty.txt
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_unpack_successfully.txt
+	if ERRORLEVEL 1 goto err
+
 rem ожидаем нулевой код если файл содержит 255 байт одинаковых символов подряд 
 	%PROGRAM% pack test_files\input_255_insuccession.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
 	if ERRORLEVEL 1 goto err
 	fc.exe "%TEMP%\result.txt" test_files\result_255_insuccession.txt
 	if ERRORLEVEL 1 goto err
-	fc.exe "%TEMP%\output.txt" test_files\output_successfully.txt
+	fc.exe "%TEMP%\output.txt" test_files\output_pack_successfully.txt
+	if ERRORLEVEL 1 goto err
+
+	%PROGRAM% unpack test_files\result_255_insuccession.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\result.txt" test_files\input_255_insuccession.txt
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_unpack_successfully.txt
 	if ERRORLEVEL 1 goto err
 
 rem ожидаем нулевой код если файл содержит 256 байт одинаковых символов подряд 
@@ -14,7 +36,14 @@ rem ожидаем нулевой код если файл содержит 256 байт одинаковых символов подряд
 	if ERRORLEVEL 1 goto err
 	fc.exe "%TEMP%\result.txt" test_files\result_256_insuccession.txt
 	if ERRORLEVEL 1 goto err
-	fc.exe "%TEMP%\output.txt" test_files\output_successfully.txt
+	fc.exe "%TEMP%\output.txt" test_files\output_pack_successfully.txt
+	if ERRORLEVEL 1 goto err
+
+	%PROGRAM% unpack test_files\result_256_insuccession.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\result.txt" test_files\input_256_insuccession.txt
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_unpack_successfully.txt
 	if ERRORLEVEL 1 goto err
 
 rem ожидаем нулевой код если файл содержит 257 байт одинаковых символов подряд 
@@ -22,13 +51,14 @@ rem ожидаем нулевой код если файл содержит 257 байт одинаковых символов подряд
 	if ERRORLEVEL 1 goto err
 	fc.exe "%TEMP%\result.txt" test_files\result_257_insuccession.txt
 	if ERRORLEVEL 1 goto err
-	fc.exe "%TEMP%\output.txt" test_files\output_successfully.txt
+	fc.exe "%TEMP%\output.txt" test_files\output_pack_successfully.txt
 	if ERRORLEVEL 1 goto err
 
-rem ожидаем ненулевой код если файл содержит символ с кодом 255
-	%PROGRAM% pack test_files\input_alpha_255.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
-	if NOT ERRORLEVEL 1 goto err
-	fc.exe "%TEMP%\output.txt" test_files\output_alpha_error.txt
+	%PROGRAM% unpack test_files\result_257_insuccession.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\result.txt" test_files\input_257_insuccession.txt
+	if ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_unpack_successfully.txt
 	if ERRORLEVEL 1 goto err
 
 rem ожидаем ненулевой код если длина запакованного файла нечётная
@@ -37,24 +67,22 @@ rem ожидаем ненулевой код если длина запакованного файла нечётная
 	fc.exe "%TEMP%\output.txt" test_files\output_odd_error.txt
 	if ERRORLEVEL 1 goto err
 
-rem ожидаем ненулевой код если в запакованном файле кол-во повторений символа, равное нулю
-	%PROGRAM% unpack test_files\input_alpha_zero_pack.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
-	if NOT ERRORLEVEL 1 goto err
-	fc.exe "%TEMP%\output.txt" test_files\output_zero_pack_error.txt
-	if ERRORLEVEL 1 goto err
-
-rem ожидаем ненулевой код если файл нулевой длины
-	%PROGRAM% pack test_files\input_file_empty.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
-	if NOT ERRORLEVEL 1 goto err
-	fc.exe "%TEMP%\result.txt" test_files\input_file_empty.txt
-	if ERRORLEVEL 1 goto err
-	fc.exe "%TEMP%\output.txt" test_files\output_empty_file.txt
-	if ERRORLEVEL 1 goto err
-
 rem ожидаем ненулевой код если нет аргумента
 	%PROGRAM% test_files\input_255_insuccession.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
 	if NOT ERRORLEVEL 1 goto err
 	fc.exe "%TEMP%\output.txt" test_files\output_argv_error.txt
+	if ERRORLEVEL 1 goto err
+
+rem ожидаем ненулевой код если аргумент не корректный
+	%PROGRAM% unpock test_files\input_255_insuccession.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if NOT ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_rle_mode_error.txt
+	if ERRORLEVEL 1 goto err
+
+rem ожидаем ненулевой код если в запакованном файле кол-во повторений символа, равное нулю
+	%PROGRAM% unpack test_files\input_alpha_zero_pack.txt "%TEMP%\result.txt" > "%TEMP%\output.txt"
+	if NOT ERRORLEVEL 1 goto err
+	fc.exe "%TEMP%\output.txt" test_files\output_zero_pack_error.txt
 	if ERRORLEVEL 1 goto err
 
 echo Program testing succeeded
