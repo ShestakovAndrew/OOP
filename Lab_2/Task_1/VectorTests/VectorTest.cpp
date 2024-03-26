bb3918d63c86f0e75310804b2fb0da38c9b7fdce
Andrew <globalshow6@gmail.com>
2024-03-26 22:00:50 +0300

    VectorTests: переписал тесты на boost

--- Lab_2/Task_1/VectorTests/VectorTest.cpp
+++ Lab_2/Task_1/VectorTests/VectorTest.cpp
@@ -1,10 +1,14 @@
-#define CATCH_CONFIG_MAIN
-#include <catch.hpp>
-#include "Vector/Vector.h"
+#define BOOST_TEST_MODULE VectorTests
+#include <boost/test/included/unit_test.hpp>
+
+#include "../Vector/Vector.h"
 
 namespace
 {
-	bool TwoVectorsEqual(std::vector<double> const& result, std::vector<double> const& changeable)
+	bool IsVectorsEqual(
+		std::vector<double> const& result, 
+		std::vector<double> const& changeable
+	)
 	{
 		return std::equal(changeable.begin(), changeable.end(), result.begin(),
 			[](double const& value1, double const& value2)
@@ -14,69 +18,66 @@ namespace
 			});
 	}
 
-	void RequireTwoDoubleVectors(std::vector<double> changeable, std::vector<double> const& result)
+	void RequireTwoDoubleVectors(
+		std::vector<double> changeable, 
+		std::vector<double> const& result
+	)
 	{
 		AddAveragePositiveNumberForAllElementsIn(changeable);
-		REQUIRE(TwoVectorsEqual(result, changeable));
+		BOOST_REQUIRE(IsVectorsEqual(result, changeable));
 	}
 }
 
-TEST_CASE("Null vector should be equal null vector.")
+BOOST_AUTO_TEST_SUITE(VectorTests)
+
+BOOST_AUTO_TEST_CASE(NullVectorShouldBeEqualNullVector)
 {
-	RequireTwoDoubleVectors({}, {});
+	RequireTwoDoubleVectors(
+		{}, 
+		{}
+	);
 }
 
-TEST_CASE("Vector should be not changed.")
+BOOST_AUTO_TEST_CASE(VectorShouldBeNotChanged)
 {
-	SECTION("If elements zero.")
-	{
-		RequireTwoDoubleVectors(
-			{ 0, 0 },
-			{ 0, 0 }
-		);
-	}
+	BOOST_TEST_MESSAGE("If elements zero.");
+	RequireTwoDoubleVectors(
+		{ 0, 0 },
+		{ 0, 0 }
+	);
 
-	SECTION("If all elements int negative.")
-	{
-		RequireTwoDoubleVectors(
-			{ -3, -2, -1, -2, -1, -2, -3 },
-			{ -3, -2, -1, -2, -1, -2, -3 }
-		);
-	}
+	BOOST_TEST_MESSAGE("If all elements int negative.");
+	RequireTwoDoubleVectors(
+		{ -3, -2, -1, -2, -1, -2, -3 },
+		{ -3, -2, -1, -2, -1, -2, -3 }
+	);
 
-	SECTION("If all elements double negative.")
-	{
-		RequireTwoDoubleVectors(
-			{ -3.24632, -2.43242, -1.47237, 0, -1.4328, -2.24632, -3.24632 },
-			{ -3.24632, -2.43242, -1.47237, 0, -1.4328, -2.24632, -3.24632 }
-		);
-	}
+	BOOST_TEST_MESSAGE("If all elements double negative.");
+	RequireTwoDoubleVectors(
+		{ -3.24632, -2.43242, -1.47237, 0, -1.4328, -2.24632, -3.24632 },
+		{ -3.24632, -2.43242, -1.47237, 0, -1.4328, -2.24632, -3.24632 }
+	);
 }
 
-TEST_CASE("Vector should be change.")
+BOOST_AUTO_TEST_CASE(VectorShouldBeChange)
 {
-	SECTION("If element positive.")
-	{
-		RequireTwoDoubleVectors(
-			{ 1.9 },
-			{ 3.8 }
-		);
-	}
+	BOOST_TEST_MESSAGE("If element positive.");
+	RequireTwoDoubleVectors(
+		{ 1.9 },
+		{ 3.8 }
+	);
 
-	SECTION("If elements contain int positive.")
-	{
-		RequireTwoDoubleVectors(
-			{ -3, -2, -1, 0, 1, 2, 3 },
-			{ -1, 0, 1, 2, 3, 4, 5 }
-		);
-	}
+	BOOST_TEST_MESSAGE("If elements contain int positive.");
+	RequireTwoDoubleVectors(
+		{ -3, -2, -1, 0, 1, 2, 3 },
+		{ -1, 0, 1, 2, 3, 4, 5 }
+	);
 
-	SECTION("If elements contain double positive.")
-	{
-		RequireTwoDoubleVectors(
-			{ -3.246, -2.432, -1.472, 0, 1.432, 2.246, 3.246 },
-			{ -0.938, -0.124, 0.836, 2.308, 3.74, 4.554, 5.554 }
-		);
-	}
+	BOOST_TEST_MESSAGE("If elements contain double positive.");
+	RequireTwoDoubleVectors(
+		{ -3.246, -2.432, -1.472, 0, 1.432, 2.246, 3.246 },
+		{ -0.938, -0.124, 0.836, 2.308, 3.74, 4.554, 5.554 }
+	);
 }
 
+BOOST_AUTO_TEST_SUITE_END()
